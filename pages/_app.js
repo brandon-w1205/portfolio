eaa70e1da3414aa43a665daa7c1fc2943ebe4f4e
brandon-w1205 <bwu1205@gmail.com>
2022-10-23 18:18:56 -0700

    reverts commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,15 @@
 import '../styles/globals.css'
 import NavBar from '../components/navBar'
-import Footer from '../components/footer'
-import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider defaultTheme='dark' attribute='class'>
+    <>
       {/* header */}
       <NavBar />
       {/* aside... */}
       <Component {...pageProps} />
       {/* footer */}
-    </ThemeProvider>
+    </>
   )
 }
 
