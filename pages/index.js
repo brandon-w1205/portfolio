f9f900cb7fd263f4fee2514fe593eb23da5d0e13
brandon-w1205 <bwu1205@gmail.com>
2022-11-06 13:25:48 -0800

    fixes favicon logo

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/favicon.png' />
         <title>Brandon Wu</title>
       </Head>
       
